6c455c2a955e02124d5a7edddaeac2f11ca91ada
1A3Dev <mcplayer1a3@gmail.com>
2025-12-01 17:25:07 +0000

    additional changes to support photon server 4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 1.0.1
+
+### NEW
+
+- Added support for Photon Server 4
+
 # 1.0.0
 
 ### CHANGES
--- source/Plugin.cs
+++ source/Plugin.cs
@@ -2,6 +2,7 @@
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
+using ExitGames.Client.Photon;
 using HarmonyLib;
 using Photon.Pun;
 using Photon.Realtime;
@@ -24,6 +25,8 @@ namespace PhotonServerSettings
         internal static ConfigEntry<string> PhotonAppIdVoice;
         internal static ConfigEntry<string> PhotonServerAddress;
         internal static ConfigEntry<int> PhotonServerPort;
+        internal static ConfigEntry<int> PhotonServerVersion;
+        internal static ConfigEntry<string> PhotonConnectionProtocol;
 
         private void Awake()
         {
@@ -37,7 +40,9 @@ namespace PhotonServerSettings
             PhotonAppIdRealtime = Config.Bind("Photon", "AppId Realtime", "", new ConfigDescription("Photon Realtime App ID"));
             PhotonAppIdVoice = Config.Bind("Photon", "AppId Voice", "", new ConfigDescription("Photon Voice App ID"));
             PhotonServerAddress = Config.Bind("Photon", "Server", "", new ConfigDescription("Photon Server Address"));
-            PhotonServerPort = Config.Bind("Photon", "Server Port", 0, new ConfigDescription("Photon Server Port"));
+            PhotonServerPort = Config.Bind("Photon", "Server Port", 0, new ConfigDescription("Photon Server Port", new AcceptableValueRange<int>(0, 65535)));
+            PhotonServerVersion = Config.Bind("Photon", "Server Version", 5, new ConfigDescription("Photon Server Version", new AcceptableValueRange<int>(4, 5)));
+            PhotonConnectionProtocol = Config.Bind("Photon", "Protocol", "Udp", new ConfigDescription("Photon Protocol", new AcceptableValueList<string>(Enum.GetNames(typeof(ConnectionProtocol)))));
 
             harmony.PatchAll(typeof(GeneralPatches));
             
@@ -60,23 +65,50 @@ namespace PhotonServerSettings
         [HarmonyPostfix]
         [HarmonyWrapSafe]
         public static void DataDirector_PhotonSetAppId_Postfix(){
+            PhotonNetwork.PhotonServerSettings.AppSettings.Port = 0;
+            PhotonNetwork.PhotonServerSettings.AppSettings.UseNameServer = true;
+            PhotonNetwork.NetworkingClient.SerializationProtocol = SerializationProtocol.GpBinaryV18;
+            
             if(!string.IsNullOrEmpty(PluginLoader.PhotonServerAddress.Value)){
                 PhotonNetwork.PhotonServerSettings.AppSettings.Server = PluginLoader.PhotonServerAddress.Value;
-                PluginLoader.StaticLogger.LogInfo("Photon: Changed Server Address");
+                PluginLoader.StaticLogger.LogInfo($"Photon: Changed Server Address");
+                
+                if(PluginLoader.PhotonServerVersion.Value == 4){
+                    PhotonNetwork.PhotonServerSettings.AppSettings.UseNameServer = false;
+                    PhotonNetwork.NetworkingClient.SerializationProtocol = SerializationProtocol.GpBinaryV16;
+                }
+                
+                if(PluginLoader.PhotonServerPort.Value > 0){
+                    PhotonNetwork.PhotonServerSettings.AppSettings.Port = PluginLoader.PhotonServerPort.Value;
+                    PluginLoader.StaticLogger.LogInfo($"Photon: Changed Server Port");
+                }
             }
-            if(PluginLoader.PhotonServerPort.Value > 0){
-                PhotonNetwork.PhotonServerSettings.AppSettings.Port = PluginLoader.PhotonServerPort.Value;
-                PluginLoader.StaticLogger.LogInfo("Photon: Changed Server Port");
+            
+            if(Enum.TryParse<ConnectionProtocol>(PluginLoader.PhotonConnectionProtocol.Value, out var protocol)){
+                PhotonNetwork.PhotonServerSettings.AppSettings.Protocol = protocol;
+                PluginLoader.StaticLogger.LogInfo($"Photon: Changed Protocol ({protocol})");
+            }else{
+                PhotonNetwork.PhotonServerSettings.AppSettings.Protocol = ConnectionProtocol.Udp;
             }
             
             if(!string.IsNullOrEmpty(PluginLoader.PhotonAppIdRealtime.Value)){
                 PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime = PluginLoader.PhotonAppIdRealtime.Value;
-                PluginLoader.StaticLogger.LogInfo("Photon: Changed AppIdRealtime");
+                PluginLoader.StaticLogger.LogInfo($"Photon: Changed AppIdRealtime");
             }
             
             if(!string.IsNullOrEmpty(PluginLoader.PhotonAppIdVoice.Value)){
                 PhotonNetwork.PhotonServerSettings.AppSettings.AppIdVoice = PluginLoader.PhotonAppIdVoice.Value;
-                PluginLoader.StaticLogger.LogInfo("Photon: Changed AppIdVoice");
+                PluginLoader.StaticLogger.LogInfo($"Photon: Changed AppIdVoice");
+            }
+        }
+        
+        [HarmonyPatch(typeof(DataDirector), nameof(DataDirector.PhotonSetRegion))]
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        public static void DataDirector_PhotonSetRegion_Postfix(){
+            if(!string.IsNullOrEmpty(PluginLoader.PhotonServerAddress.Value) && PluginLoader.PhotonServerVersion.Value == 4){
+                PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "";
+                PluginLoader.StaticLogger.LogInfo("Photon: Cleared Region");
             }
         }
         
@@ -100,7 +132,7 @@ namespace PhotonServerSettings
         public static void SteamManager_SendSteamAuthTicket_Postfix(){
             if(!string.IsNullOrEmpty(PluginLoader.PhotonAppIdRealtime.Value) || !string.IsNullOrEmpty(PluginLoader.PhotonAppIdVoice.Value)){
                 PhotonNetwork.AuthValues.AuthType = CustomAuthenticationType.None;
-                PluginLoader.StaticLogger.LogInfo("Photon: Cleared Steam Auth Ticket");
+                PluginLoader.StaticLogger.LogInfo("Photon: Cleared Auth Type");
             }
         }
     }
