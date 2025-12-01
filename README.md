c630236cc4b491c20100bbc931d5b8e10ada0431
1A3Dev <mcplayer1a3@gmail.com>
2025-12-01 14:13:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# PhotonServerSettings
+
+[![Latest Version](https://img.shields.io/thunderstore/v/Dev1A3/PhotonServerSettings_REPO?style=for-the-badge&logo=thunderstore&logoColor=white)](https://thunderstore.io/c/repo/p/Dev1A3/PhotonServerSettings_REPO)
+[![Total Downloads](https://img.shields.io/thunderstore/dt/Dev1A3/PhotonServerSettings_REPO?style=for-the-badge&logo=thunderstore&logoColor=white)](https://thunderstore.io/c/repo/p/Dev1A3/PhotonServerSettings_REPO)
+[![Discord](https://img.shields.io/discord/646323142737788928?style=for-the-badge&logo=discord&logoColor=white&label=Discord)](https://discord.gg/CKqVFPRtKp)
+[![Ko-fi](https://img.shields.io/badge/Donate-F16061.svg?style=for-the-badge&logo=ko-fi&logoColor=white&label=Ko-fi)](https://ko-fi.com/K3K8SOM8U)
+
+### Information
+
+This mod allows you to override a few values to allow using a custom photon server:
+
+- Steam App ID
+- Photon Realtime App ID
+- Photon Voice App ID
+- Photon Server Address
+
+### Support
+
+You can get support in any the following places:
+
+- The [thread](https://discord.com/channels/1344557689979670578/1391111846823465082) in the [REPO Modding Discord Server](https://discord.gg/repomodding)
+- [GitHub Issues](https://github.com/1A3Dev/REPO-PhotonServerSettings/issues)
+- [My Discord Server](https://discord.gg/CKqVFPRtKp)
+
+### Compatibility
+
+- Supported Game Versions:
+  - v0.3.x
+- Not Compatible With:
+  - N/A
---
+++ source/Plugin.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+using Steamworks;
+
+namespace PhotonServerSettings
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    internal class PluginLoader : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        private static bool initialized;
+
+        internal static ManualLogSource StaticLogger { get; private set; }
+        internal static ConfigFile StaticConfig { get; private set; }
+
+        internal static ConfigEntry<string> SteamAppId;
+        internal static ConfigEntry<string> PhotonAppIdRealtime;
+        internal static ConfigEntry<string> PhotonAppIdVoice;
+        internal static ConfigEntry<string> PhotonServer;
+
+        private void Awake()
+        {
+            if (initialized) return;
+            initialized = true;
+
+            StaticLogger = Logger;
+            StaticConfig = Config;
+            
+            SteamAppId = Config.Bind("Steam", "AppId", "", new ConfigDescription("Steam App ID"));
+            PhotonAppIdRealtime = Config.Bind("Photon", "AppId Realtime", "", new ConfigDescription("Photon Realtime App ID"));
+            PhotonAppIdVoice = Config.Bind("Photon", "AppId Voice", "", new ConfigDescription("Photon Voice App ID"));
+            PhotonServer = Config.Bind("Photon", "Server", "", new ConfigDescription("Photon Server Address"));
+
+            harmony.PatchAll(typeof(GeneralPatches));
+            
+            StaticLogger.LogInfo("Patches Loaded");
+        }
+
+#if DEBUG
+        private void OnDestroy()
+        {
+            harmony?.UnpatchSelf();
+            StaticLogger.LogInfo("Patches Unloaded");
+        }
+#endif
+    }
+
+    [HarmonyPatch]
+    internal static class GeneralPatches
+    {
+        [HarmonyPatch(typeof(DataDirector), nameof(DataDirector.PhotonSetAppId))]
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        public static void DataDirector_PhotonSetAppId_Postfix(){
+            if(!string.IsNullOrEmpty(PluginLoader.PhotonServer.Value)){
+                PhotonNetwork.PhotonServerSettings.AppSettings.Server = PluginLoader.PhotonServer.Value;
+                PluginLoader.StaticLogger.LogInfo("Photon: Changed Server");
+            }
+            
+            if(!string.IsNullOrEmpty(PluginLoader.PhotonAppIdRealtime.Value)){
+                PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime = PluginLoader.PhotonAppIdRealtime.Value;
+                PluginLoader.StaticLogger.LogInfo("Photon: Changed AppIdRealtime");
+            }
+            
+            if(!string.IsNullOrEmpty(PluginLoader.PhotonAppIdVoice.Value)){
+                PhotonNetwork.PhotonServerSettings.AppSettings.AppIdVoice = PluginLoader.PhotonAppIdVoice.Value;
+                PluginLoader.StaticLogger.LogInfo("Photon: Changed AppIdVoice");
+            }
+        }
+        
+        [HarmonyPatch(typeof(SteamManager), nameof(SteamManager.Awake))]
+        [HarmonyPrefix]
+        [HarmonyWrapSafe]
+        public static void SteamManager_Awake_Prefix(){
+            if(!SteamManager.instance && uint.TryParse(PluginLoader.SteamAppId.Value, out uint steamAppId)){
+                try {
+                    SteamClient.Init(steamAppId);
+                    PluginLoader.StaticLogger.LogInfo($"Steam: Changed AppId to {steamAppId}");
+                }catch(Exception ex){
+                    PluginLoader.StaticLogger.LogError("Steamworks failed to initialize. Error: " + ex.Message);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(SteamManager), nameof(SteamManager.SendSteamAuthTicket))]
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        public static void SteamManager_SendSteamAuthTicket_Postfix(){
+            if(!string.IsNullOrEmpty(PluginLoader.PhotonAppIdRealtime.Value) || !string.IsNullOrEmpty(PluginLoader.PhotonAppIdVoice.Value)){
+                PhotonNetwork.AuthValues.AuthType = CustomAuthenticationType.None;
+                PluginLoader.StaticLogger.LogInfo("Photon: Cleared Steam Auth Ticket");
+            }
+        }
+    }
+}
